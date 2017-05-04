1da601ce7f38b3d4c061c20f68327ff61153bf24
ben harvey <benjamin_harvey@benharveysMini.attlocal.net>
2017-05-03 20:29:51 -0400

    Working grid. Added clear button to nav.

---
+++ sketch.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+	for (i=0; i<4096; i++) {
+	$("<div>", {class: 'col'}).appendTo($(".container"));
+
+	// $(".container").append("<div>");
+	// $('div div').addClass("col");
+}
+	$(".col").hover(function() {
+		$(this).css("background-color", "blue");
+	}, function() {
+		$(this).css("background-color", "blue");
+	});
+
+	});
+
