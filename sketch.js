004e132744db70bbbd96c9a4ec65ac286b99041f
ben harvey <benjamin_harvey@benharveysMini.attlocal.net>
2017-05-04 16:48:04 -0400

    Added functionality to resize button

--- sketch.js
+++ sketch.js
@@ -1,21 +1,55 @@
-$(document).ready(function() {
-	
-	for (i=0; i<4096; i++) {
-	$("<div>", {class: 'col'}).appendTo($(".container"));
 
-	// $(".container").append("<div>");
-	// $('div div').addClass("col");
-}
-	$(".col").hover(function() {
-		$(this).css("background-color", "blue");
-	}, function() {
-		$(this).css("background-color", "blue");
-	});
+
+
+$(document).ready(function() {
+
+	var num = 4;
+	
+	makeGrid(num);
+
+	function makeGrid(num) {	
+		for (i = 0; i < num * num; i++) {
+		$("<div>", {class: 'col'}).appendTo($(".container"))
+		};
+		makeSquare();
+		$(".col").hover(function() {
+			$(this).css("background-color", "blue");
+			}, function() {
+			$(this).css("background-color", "blue");
+		});
+
+	}
+	
+	function getInput() {
+		num = prompt('Please enter a number from 1-64 to resize the grid.');
+		console.log(num);
+		return num;
+	}
+	
+	function makeSquare() {
+		square = 800 / num
+		
+		$('.col').css('height', square + 'px');
+		$('.col').css('width', square + 'px');
+	}
 	
 	$('#clear').click(function () {
-		$('.col').css('background-color', 'white');
+		$('.col').css('background-color', 'white');	
+	});
 	
+	$('#resize').click(function () {
+		getInput();
+		$('.col').remove();
+		makeGrid(num);
 	});
+	
 
-	});
+	
 
+	
+	
+});
+	
+
+
+		
